a3450ca851fc4344597afca9662c76a6a948e9b9
ramyamorla <morlaramyaasri@gmail.com>
2025-04-09 23:31:26 -0400

    Conected Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FirebaseMessaging.onBackgroundMessage(_messageHandler);
-  runApp(MessagingTutorial());
+  runApp(MessagingTutorial());//
 }
 
 class MessagingTutorial extends StatelessWidget {
